9f758083abd713b0a7c4ba9a02c587618aef457a
Himsharma-code <himanshu15101998@gmail.com>
2023-12-03 13:28:13 +0530

    fix: mobile design

--- src/layouts/PrivateLayout/PrivateLayout.js
+++ src/layouts/PrivateLayout/PrivateLayout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { privateRoutes } from "src/routes";
+import classes from "./PrivateLayout.module.scss";
 
 const PrivateLayout = () => {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <nav>
         <h1>Wordle</h1>
       </nav>
